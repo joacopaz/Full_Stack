533d6d19783d9361d0f1f8649316f0b4d4a8349b
joacopaz <joacopaz@gmail.com>
2022-06-30 18:30:47 -0300

    updated promises

--- Notes/JS/promises.js
+++ Notes/JS/promises.js
@@ -1,4 +1,4 @@
-// Promises resolve or fail, we can construct a promise as follows:
+// Promises resolve or fail, we can construct a promise as follows, the function always needs two parameters, resolve and reject:
 
 const someCondition = true
 
@@ -23,8 +23,7 @@ const returnPromiseFunction = () => {
 
 let prom = returnPromiseFunction();
 
-// When the promise settles (it starts pending) we can .then() tell it what to do, then() receives 2 callbacks as arguments (resolve, rejected)
-
+// When the promise settles (it starts pending) we can .then() handle it and tell it what to do, then() receives 2 callbacks as arguments (resolve, reject), if only 1 is given no reject handling will be applied
 prom = new Promise((resolve, reject) => {
     let num = Math.random();
     if (num < .5) {
@@ -43,3 +42,37 @@ const handleFailure = (rejectionReason) => {
 };
 
 prom.then(handleSuccess, handleFailure);
+
+// We can use catch on a then() chain to get the errors, catch takes only one argument onRejected.
+
+prom
+    .then((resolvedValue) => {
+        console.log(resolvedValue);
+    })
+    .catch((rejectionReason) => {
+        console.log(rejectionReason);
+    });
+
+// You can chain promises to make sure one thing goes after the other:
+
+firstPromiseFunction()
+    .then((firstResolveVal) => {
+        return secondPromiseFunction(firstResolveVal); // we need to return for the third execution to receive a value
+    })
+    .then((secondResolveVal) => {
+        console.log(secondResolveVal);
+    });
+
+// 2 common mistakes: forgetting to return the then() value and nesting (not closing properly the thens)
+
+Promise.all(['An array of promises']) // this is good when we don't care the order in which the promises resolve, it returns a single promise, if all resolved an array with resolved values, if at least one didn't resolve it will reject with the reason of rejection
+
+let myPromises = Promise.all([returnsPromOne(), returnsPromTwo(), returnsPromThree()]);
+ 
+myPromises
+  .then((arrayOfValues) => {
+    console.log(arrayOfValues);
+  })
+  .catch((rejectionReason) => {
+    console.log(rejectionReason);
+  });
